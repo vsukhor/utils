7147f0377b954ad13427a0e76c195097551a96e4
Valerii Sukhorukov <29120451+vsukhor@users.noreply.github.com>
2021-06-06 11:26:08 +0200

    try..catch in check_range

--- utils/config/parameter/scalars.h
+++ utils/config/parameter/scalars.h
@@ -193,7 +193,12 @@ Par( const std::string& name,
     : Base<T> {check_name(name)}
 {
     this->load(file);
-    check_range(range, msgr);
+    try {
+        check_range(range, msgr);
+    }
+    catch (exceptions::ParOutOfRange<T,isDiscrete> e) {
+        std::exit(EXIT_FAILURE);
+    }
     print(msgr);
 }
 
@@ -207,7 +212,12 @@ Par( const std::string& name,
     : Base<T> {check_name(name)}
 {
     this->load(file);
-    check_range(range, msgr);
+    try {
+        check_range(range, msgr);
+    }
+    catch (exceptions::ParOutOfRange<T,isDiscrete> e) {
+        std::exit(EXIT_FAILURE);
+    }
     print(msgr);
 }
 
